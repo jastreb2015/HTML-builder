652e1e5cbb5a0def05d132a6430ca1b403dc04ef
jastreb2015 <82231229+jastreb2015@users.noreply.github.com>
2022-11-08 02:42:22 +0400

    feat: add handler of signal to 02-write-file/index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,34 +1,39 @@
 'use strict'
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout } = process;
-// const input = fs.createReadStream('./02-write-file/text.txt', 'utf-8');
+const {
+  stdin,
+  stdout
+} = process;
 
 fs.writeFile(
-    path.join('02-write-file', 'text.txt'),
-    '',
-    (err) => {
-        if (err) throw err;
-        console.log('Hello! File was created. Add some notes to it.');
-    }
+  path.join('02-write-file', 'text.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+    console.log('Hello! File was created. Add some notes to it.');
+  }
 );
 
-stdin.on('data', data => 
-{
- let str = data.toString();
+stdin.on('data', data => {
+  let str = data.toString();
 
-    if (str.indexOf('exit') !== -1) {
-        process.exit();
-} else {
+  if (str.indexOf('exit') !== -1) {
+    process.exit();
+  } else {
     fs.appendFile(
-        path.join('02-write-file', 'text.txt'),
-        data,
-        err => {
-            if (err) throw err;
-            console.log('Файл был изменен');
-        }
+      path.join('02-write-file', 'text.txt'),
+      data,
+      err => {
+        if (err) throw err;
+        console.log('Файл был изменен');
+      }
     )
-}
+  }
+  process.on('SIGINT', () => {
+    process.exit()
+  });
 });
+
 process.on('exit', () => stdout.write('Удачи в изучении Node.js!\n'));
 process.on('exit', () => console.log('Good luck!'));
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,44 +2,45 @@ const fs = require('fs');
 const dirPath = '04-copy-directory/files';
 const dirCopyPath = '04-copy-directory/files-copy';
 
-fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => {
-    if (err) throw err;
-}
-);
+fs.mkdir('04-copy-directory/files-copy', {
+  recursive: true
+}, (err) => {
+  if (err) throw err;
+});
 
 fs.readdir(dirCopyPath, {
-    withFileTypes: true
-  }, (err, files) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    for (const file of files) {
-      let fileCopyPath = dirCopyPath + '/' + file.name;
-      fs.unlink(fileCopyPath, err => {
-        if(err) throw err;
-        console.log('Файл успешно удален');
-     });
+  withFileTypes: true
+}, (err, files) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  for (const file of files) {
+    let fileCopyPath = dirCopyPath + '/' + file.name;
+    fs.unlink(fileCopyPath, err => {
+      if (err) throw err;
+      console.log('Файл успешно удален');
+    });
 
-    }
-  
-  });
+  }
+
+});
 
 fs.readdir(dirPath, {
-    withFileTypes: true
-  }, (err, files) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    for (const file of files) {
-      let filePath = dirPath + '/' + file.name;
-      let fileCopyPath = dirCopyPath + '/' + file.name;
-      fs.copyFile(filePath, fileCopyPath, err => {
-        if(err) throw err;
-        console.log('Файл успешно скопирован');
-     });
+  withFileTypes: true
+}, (err, files) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  for (const file of files) {
+    let filePath = dirPath + '/' + file.name;
+    let fileCopyPath = dirCopyPath + '/' + file.name;
+    fs.copyFile(filePath, fileCopyPath, err => {
+      if (err) throw err;
+      console.log('Файл успешно скопирован');
+    });
 
-    }
-  
-  });
+  }
+
+});
